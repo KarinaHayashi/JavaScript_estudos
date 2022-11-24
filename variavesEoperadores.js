25df8844ccc18ec4ffd5c5f873ac14f8c7032aa3
Karina Hayashi <100161303+KarinaHayashi@users.noreply.github.com>
2022-11-23 21:19:27 -0300

    Add files via upload
    Aula 01 sobre variáveis e operadores

---
+++ variavesEoperadores.js
@@ -0,0 +1,19 @@
+
+/*let variavel =10; o let posso mudar o valor dela
+const pi = 3.14;  const é um valor fixo
+console.log("Ola mundo") 
+
+let variavel = 10 + 11;
+
+console.log(variavel);
+ */
+
+//Primeiro desafio:
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto = litrosConsumidos * precoCombustivel;
+
+console.log(valorGasto);
