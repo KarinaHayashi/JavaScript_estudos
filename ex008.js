d2fd714738866e9795767fc744c69a60e88b4a89
Karina Hayashi <100161303+KarinaHayashi@users.noreply.github.com>
2022-12-08 20:58:44 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,10 @@
+/*Crie um programa que dado um número imprima a sua tabuada
+ A tabuada vai até o 10 o 10 é um número fixo */
+
+const numero = 7;
+for (let i = 1; i <= 10; i++) {
+    console.log(i * numero);
+
+}
+
+
