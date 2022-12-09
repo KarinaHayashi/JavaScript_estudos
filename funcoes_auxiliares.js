ff8cd657184c262411f29eb007525c27e9f1863b
Karina Hayashi <100161303+KarinaHayashi@users.noreply.github.com>
2022-12-09 00:07:59 -0300

    Add files via upload

---
+++ funcoes_auxiliares.js
@@ -0,0 +1,14 @@
+/* Como organizar os nossos códigos
+Vamos trabalhar com importação de arquivo*/
+
+function gets(){
+    return 10;
+
+}
+
+function print(texto){
+    console.log(texto);
+}
+
+
+module.exports = {gets, print}
---
+++ main.js
@@ -0,0 +1,5 @@
+
+
+const funcoes = require ('./funcoes_auxiliares');
+
+console.log(funcoes.gets());
